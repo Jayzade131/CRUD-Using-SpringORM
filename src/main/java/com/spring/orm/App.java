27571bab1b386483045f209dc92c05f0b0012506
jay zade <jaykumarzade@gmail.com>
2023-04-24 14:55:11 +0530

    update

--- src/main/java/com/spring/orm/App.java
+++ src/main/java/com/spring/orm/App.java
@@ -1,5 +1,11 @@
 package com.spring.orm;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.orm.dao.StudentDao;
+import com.spring.orm.entities.Student;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "start process..." );
+        ApplicationContext context=new ClassPathXmlApplicationContext("com/spring/orm/config.xml");
+        StudentDao studentDao= context.getBean("StudentDao",StudentDao.class);
+        Student student=new Student(111,"mahesh","nagpur");
+        int r=studentDao.insert(student);
+        System.out.println("done "+r);
     }
 }
--- src/main/java/com/spring/orm/dao/StudentDao.java
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.spring.orm.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import com.spring.orm.entities.Student;
@@ -7,8 +9,20 @@ import com.spring.orm.entities.Student;
 public class StudentDao {
 
 	private HibernateTemplate hibernatetemplate;
+	 	
+
+	public HibernateTemplate getHibernatetemplate() {
+		return hibernatetemplate;
+	}
+
+
+	public void setHibernatetemplate(HibernateTemplate hibernatetemplate) {
+		this.hibernatetemplate = hibernatetemplate;
+	}
+
 
 	// INSERT
+	@Transactional
 	public int insert(Student student) {
 
 		int i = (Integer) this.hibernatetemplate.save(student);
